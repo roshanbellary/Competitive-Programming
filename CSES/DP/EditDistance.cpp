cecf60e93ff1a58a412465ca367397420bd860ff
roshanbellary <40147115+roshanbellary@users.noreply.github.com>
2021-09-23 23:54:39 -0700

    Edit Distance Solution

--- CSES/DP/EditDistance.cpp
+++ CSES/DP/EditDistance.cpp
@@ -24,7 +24,19 @@ string a,b;
 int main(){
 	setIO();
 	cin >> a >> b;
-	int N = a.length();
-	int M = b.length();
-	int dp[N];
+	int dp[a.length()+1][b.length()+1];
+	for (int i=0;i<=a.length();i++){
+		for (int j=0;j<=b.length();j++){
+			if (i==0){
+				dp[i][j]=j;
+			}else if (j==0){
+				dp[i][j]=i;
+			}else if (a[i-1]==b[j-1]){
+				dp[i][j]=dp[i-1][j-1];
+			}else{
+				dp[i][j]=min(min(dp[i][j-1],dp[i-1][j]),dp[i-1][j-1])+1;
+			}
+		}
+	}
+	cout << dp[a.length()][b.length()] << "\n";
 }

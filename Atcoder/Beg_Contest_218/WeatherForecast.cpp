7a2627c2edc5aa90faa268c219fccc7a1b394e19
Roshan Bellary <roshan.bellary@gmail.com>
2021-11-22 18:24:09 -0800

    Atcoder Contest

---
+++ Atcoder/Beg_Contest_218/WeatherForecast.cpp
@@ -0,0 +1,51 @@
+/*
+Problem: 
+Problem Link: 
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+template <class T> 
+void rn(T &n){
+    n=0;int m=1,c=getchar();if (c=='-') m=-1;
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+void rw(string &w){
+    w="";char c=getchar();
+    while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}
+}
+void rc(char &c){c=getchar();}
+template<typename T, typename ...Types>
+void rn(T &n, Types&&... args){rn(n);rn(args...);}
+template<typename T, typename ...Types>
+void rw(T &w,Types&&... args){rw(w);rw(args...);}
+template<typename T, typename ...Types>
+void rc(T &c,Types&&... args){rc(c);rc(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+int N;string S;
+int main(){
+	setIO();rn(N);rw(S);
+    cout << ((S[--N]=='o')?"Yes\n":"No\n");
+}

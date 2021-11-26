848151b90f44503ccd7f81a8e118be7cdeda8547
Roshan Bellary <roshan.bellary@gmail.com>
2021-11-26 12:13:30 -0800

    Kitchen

---
+++ BalticOlympiad/2019/Kitchen.cpp
@@ -0,0 +1,93 @@
+/*
+Problem: Kitchen
+Problem Link: https://oj.uz/problem/view/BOI19_kitchen
+Notes: Cool dp problem
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar_unlocked();
+    if (c=='-'){m=-1; c=getchar_unlocked();}
+    for (;c>47 && c<58;c=getchar_unlocked()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar_unlocked();
+    if (c=='-'){m=-1; c=getchar_unlocked();}
+    for (;c>47 && c<58;c=getchar_unlocked()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar_unlocked();
+    if (c=='-'){m=-1; c=getchar_unlocked();}
+    for (;c>47 && c<58;c=getchar_unlocked()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){
+        double p = 0.1;c=getchar_unlocked();
+        for (;c>47 && c<58;c=getchar_unlocked()){n+=((c-48)*p);p/=10;}
+    }
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar_unlocked();
+    if (c=='-'){m=-1; c=getchar_unlocked();}
+    for (;c>47 && c<58;c=getchar_unlocked()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){
+        double p = 0.1;c=getchar_unlocked();
+        for (;c>47 && c<58;c=getchar_unlocked()){n+=((c-48)*p);p/=10;}
+    }
+    n*=m;
+}
+inline void rv(string &w){
+    w="";char c=getchar_unlocked();
+    while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar_unlocked();}
+}
+inline void rv(char &c){c=getchar_unlocked();}
+template<typename T, typename ...Types>
+void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+int N, M, K, a[(int)300], b[(int)300], dp[90300];
+int main(){
+	setIO();cin >> N >> M >> K;
+    for (int i=0;i<N;i++) cin >> a[i];
+    for (int i=0;i<M;i++) cin >> b[i];
+    int sum = 0;
+    int minv = 1e9;for (int i=0;i<N;i++) minv=min(minv,a[i]), sum+=a[i];
+    if (minv<K){cout << "Impossible\n";return 0;}
+    memset(dp,-1,sizeof(dp));
+    dp[0]=0;
+    for (int i=0;i<M;i++){
+        for (int j=90300-b[i]-1;j>=0;j--){
+            if (dp[j]!=(-1)){
+                dp[j+b[i]]=max(dp[j+b[i]],dp[j]+min(b[i],N));
+            }
+        }
+    }
+    int v = 1e9;
+    for (int i=sum;i<90300;i++){
+        if (dp[i]>=N*K){
+            v=min(v,i-sum);
+        }
+    }
+    cout << ((v==1e9)?"Impossible":to_string(v)) << "\n";
+}

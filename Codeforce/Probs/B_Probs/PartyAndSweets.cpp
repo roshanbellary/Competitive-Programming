be9dd1d12a40fdd191c204a76ca06e6f9db5039c
Roshan Bellary <roshan.bellary@gmail.com>
2021-12-04 13:25:52 -0800

    Codeforce Solutions

---
+++ Codeforce/Probs/B_Probs/PartyAndSweets.cpp
@@ -0,0 +1,83 @@
+/*
+Problem: The Party and Sweets
+Problem Link: https://codeforces.com/problemset/problem/1158/A
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){
+        double p = 0.1;c=getchar();
+        for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}
+    }
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){
+        double p = 0.1;c=getchar();
+        for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}
+    }
+    n*=m;
+}
+inline void rv(string &w){
+    w="";char c=getchar();
+    while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}
+}
+inline void rv(char &c){c=getchar();}
+template<typename T, typename ...Types>
+void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 1e5;
+int N, M, b[MN], g[MN];
+ll C = 0;
+int main(){
+	setIO();rv(N, M);
+    for (int i=0;i<N;i++) rv(b[i]);
+    for (int i=0;i<M;i++) rv(g[i]);
+    sort(b,b+N);sort(g,g+M);
+    int mb=b[N-1], mg = g[0];
+    if (mg<mb){cout << "-1\n"; return 0;}
+    for (int i=0;i<M;i++) C+=g[i];
+    C+=b[N-1];C-=b[N-2];
+    for (int i=0;i<N-1;i++) C+=b[i]*(M);
+    cout << C << "\n";
+}
---
+++ Codeforce/Probs/B_Probs/USBvsPs2.cpp
@@ -0,0 +1,99 @@
+/*
+Problem: USB vs. PS/2
+Problem Link: https://codeforces.com/contest/762/problem/B
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){
+        double p = 0.1;c=getchar();
+        for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}
+    }
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){
+        double p = 0.1;c=getchar();
+        for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}
+    }
+    n*=m;
+}
+inline void rv(string &w){
+    w="";char c=getchar();
+    while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}
+}
+inline void rv(char &c){c=getchar();}
+template<typename T, typename ...Types>
+void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 3e5;
+struct Mouse{
+    ll v;int t;
+    bool operator<(const Mouse &a) const {
+        return (a.v<v);
+    }
+};
+priority_queue<Mouse> pq;
+int c[3],M, comps=0;
+ll price = 0;
+int main(){
+	setIO();rv(c[0],c[1],c[2],M);
+    for (int i=0;i<M;i++){
+        Mouse e;
+        rv(e.v);
+        string t;rv(t);
+        if (t=="USB") e.t=0;
+        else e.t=1;
+        pq.push(e);
+    }
+    while (!pq.empty()){
+        Mouse t = pq.top();pq.pop();
+        if (c[t.t]){
+            c[t.t]--;price+=t.v;comps++;
+        }else if (c[2]){
+            c[2]--;price+=t.v;comps++;
+        }
+    }
+    cout << comps << " " << price << "\n";
+
+}

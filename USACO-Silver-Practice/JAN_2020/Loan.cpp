ace7b82c4d65b43249feea59003936f24d19c152
Roshan Bellary <roshan.bellary@gmail.com>
2021-12-05 12:06:18 -0800

    Loan Solution

--- USACO-Silver-Practice/JAN_2020/Loan.cpp
+++ USACO-Silver-Practice/JAN_2020/Loan.cpp
@@ -1,34 +1,96 @@
+/*
+Problem: Loan Repayment
+Problem Link: http://usaco.org/index.php?page=viewproblem2&cpid=991
+Notes: Hard problem involving a time optimization of O(sqrt(N)) 
+where we consider how many distinct values we get overall
+*/
+#pragma GCC optimize("O2")
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-long long N, K, M;
-bool check(int x){
-    long long given = 0;
-    long long days = K;
-    while ((days>0)&&(given<N)){
-        long long y = (N-given)/x;
-        if (y<M){
-            return ((N-given+M-1)/M<=days);
-        }
-        long long increase = (N-x*y-given)/y+1;
-        if (increase>days) increase=days;
-        given+=y*increase;
-        days-=increase;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){
+        double p = 0.1;c=getchar();
+        for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}
     }
-    return (given>=N);
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){
+        double p = 0.1;c=getchar();
+        for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}
+    }
+    n*=m;
+}
+inline void rv(string &w){
+    w="";char c=getchar();
+    while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}
+}
+inline void rv(char &c){c=getchar();}
+template<typename T, typename ...Types>
+void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+ll N, K, M;
+bool check(ll X){
+    ll G = 0, k=K, n=N, m=M;
+    while (k>0 && G<n){
+        ll Y = (n-G)/X;
+        if (Y<M){
+            ll lo = (n-G+M-1)/M;
+            return (lo<=k);
+        }
+        ll mM= n-X*Y;
+        ll dy = (mM-G)/Y+1;
+        dy=min(dy,k);
+        G+=Y*dy;
+        k-=dy;
+    }
+    return (G>=n);
 }
 int main(){
-    freopen("loan.in","r",stdin);
-    freopen("loan.out","w",stdout);
-    cin >> N >> K >> M;
-    long long a = 1;
-    long long b = 1e12;
-    while (a!=b){
-        long long mid =(a+b+1)/2;
-        if (check(mid)){
-            a=mid;
-        }else{
-            b=mid-1;
-        }
+	setIO("loan");rv(N, K, M);
+    ll s = 1, f=1e12;
+    while (s!=f){
+        ll mid = (s+f+1)/2;
+        if (check(mid)) s=mid;
+        else f=mid-1;
     }
-    cout << a;
+    cout << s << "\n";
 }

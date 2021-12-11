27423e4e98bfc0c8b1bb599e745fd6918204046e
Roshan Bellary <roshan.bellary@gmail.com>
2021-12-11 12:29:07 -0800

    Mountains Solution

--- USACO-Silver-Practice/JAN_2019/Mountains.cpp
+++ USACO-Silver-Practice/JAN_2019/Mountains.cpp
@@ -1,40 +1,89 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+/*
+Problem: 
+Problem Link: 
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
-int N;
-class Range{
-    public:
-        int s,f;
-};
-vector<Range> px;
-bool sortsx(Range a, Range b){
-    return (a.s)<(b.s);
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
 }
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){
+        double p = 0.1;c=getchar();
+        for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}
+    }
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){
+        double p = 0.1;c=getchar();
+        for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}
+    }
+    n*=m;
+}
+inline void rv(string &w){
+    w="";char c=getchar();
+    while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}
+}
+inline void rv(char &c){c=getchar();}
+template<typename T, typename ...Types>
+void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+struct Range{int s,f;};
+bool sorts(Range &a, Range &b){
+    if (a.s==b.s) return a.f>b.f;
+    return a.s<b.s;
+}
+const int MN = 1e5;
+int N, M=1;
+Range r[MN];
 int main(){
-    freopen("mountains.in","r",stdin);
-    freopen("mountains.out","w",stdout);
-    cin >> N;
+	setIO("mountains");rv(N);
     for (int i=0;i<N;i++){
-        int x, y;
-        cin >> x >> y;
-        Range pe;
-        pe.s=x-y;
-        pe.f=x+y;
-        px.push_back(pe);
+        int x, y; rv(x,y);
+        r[i].s=x-y;r[i].f=x+y;
     }
-    sort(px.begin(),px.end(),sortsx);
-    int temp = 0;
-    int i = 1;
-    int number = 1;
-    while (i<N){
-        if (px[i].f<=px[temp].f){
-            i++;
-        }else{
-            number++;
-            temp=i;
-            i++;
-        }
+    sort(r,r+N,sorts);
+    int ef=r[0].f;
+    for (int i=1;i<N;i++){
+        if (ef<r[i].f) ef=r[i].f,M++;
     }
-    cout << number;
+    cout << M << "\n";
 }

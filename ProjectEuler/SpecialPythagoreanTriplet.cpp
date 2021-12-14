cdb5e4f9456b6150598e1f9ac5e7d71c227a656b
Roshan Bellary <roshan.bellary@gmail.com>
2021-12-14 08:30:05 -0800

    Project Euler Solutions

---
+++ ProjectEuler/SpecialPythagoreanTriplet.cpp
@@ -0,0 +1,17 @@
+/*
+Problem: Special Pythagorean triplet
+Problem Link: https://projecteuler.net/problem=9
+Notes: 
+No need to program this problem
+One cool thing about pythagorean triplet is that they can be expressed
+as a=m^2-n^2, b=2mn, c=m^2+n^2
+Thus we just have to find m, n st. m>n and m>m+n and 2m^2+2mn=1000
+2m^2+2mn=1000 => 2m(m+n)=1000 => m(m+n)=500
+the above equation gives us that m=20, n=5
+thus m^2-n^2=375,2mn=200, and m^2+n^2=425
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    cout << 200*375*425 << "\n";
+}

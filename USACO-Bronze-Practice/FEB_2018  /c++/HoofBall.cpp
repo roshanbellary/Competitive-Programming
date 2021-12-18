a1e5f778f31cdb44afabaacb2b7eac2b8f8ee210
Roshan Bellary <roshan.bellary@gmail.com>
2021-12-17 22:22:57 -0800

    Hoofball Solution

--- USACO-Bronze-Practice/FEB_2018  /c++/HoofBall.cpp
+++ USACO-Bronze-Practice/FEB_2018  /c++/HoofBall.cpp
@@ -1,36 +1,85 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+/*
+Problem: 
+Problem Link: 
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=getchar();}
+template<typename T, typename ...Types>
+void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 100;
+int N,p[MN],d[MN];
 int main(){
-    int N;
-    freopen("hoofball.in","r",stdin);
-    freopen("hoofball.out","w",stdout);
-    cin >> N;
-    int c[N];
-    for (int i=0;i<N;i++){
-        cin >> c[i];
-    }
-    sort(c, c+N);
-    int p[N];
-    int count = 1;
-    p[0]=1;
-    p[N-1]=N-2;
+	setIO("hoofball");rv(N);
+    for (int i=0;i<N;i++) rv(p[i]);
+    sort(p,p+N);d[0]=1;d[N-1]=-1;
     for (int i=1;i<N-1;i++){
-        if ((c[i+1]-c[i])<(c[i]-c[i-1])){
-            p[i]=i+1;
-        }else{
-            p[i]=i-1;
+        if (i!=0 && i!=N-1){
+            if (p[i+1]-p[i]<p[i]-p[i-1]) d[i]=1;
+            else d[i]=-1;
         }
     }
-    for (int i=0;i<N-1;i++){
-        if ((p[i]==i+1)&&(p[i+1]==i)){
-            if (i!=0){
-                count++;
-            }
+    int comps = 0;
+    for (int i=0;i<N;i++){
+        int j=i+1;
+        while (j<N){
+            if (d[j]==d[i]) j++;
+            else break;
         }
+        int n = j+1;
+        while (n<N){
+            if (d[n]==d[j]) n++;
+            else break;
+        }
+        if (n-j==1||j-i==1) comps++;
+        else comps+=2;
+        i=n-1;
     }
-    
-    cout << count;
-    return 0;
+    cout << comps << "\n";
 }

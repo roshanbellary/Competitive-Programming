490ad8dd204bae536bdc040c7ccd39e021ec8b9b
Roshan Bellary <roshan.bellary@gmail.com>
2021-12-20 17:18:08 -0800

    Create DynamicRangeSumQueries.cpp

---
+++ CSES/Range_Queries/DynamicRangeSumQueries.cpp
@@ -0,0 +1,78 @@
+/*
+Problem: Dynamic Range Sum Queries
+Problem Link: https://cses.fi/problemset/task/1648
+Notes: P much Fenwick Trees
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=getchar();}
+template<typename T, typename ...Types>
+void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 2e5+1;
+const int MD = 19;
+ll N, Q, sm[MN], v[MN];
+void update(int ind, ll v){
+    int i = ind;
+    while (i<=N){
+        sm[i]+=(v);i+=(i&(-i));
+    }
+}
+ll query(int ind){
+    return ((ind==0)?0:sm[ind]+query(ind-(ind&(-ind))));
+}
+int main(){
+	setIO();rv(N, Q);
+    for (int i=0;i<N;i++){
+        rv(v[i+1]);update(i+1,v[i+1]);
+    }
+    for (int i=0;i<Q;i++){
+        ll t, a, b; rv(t,a,b);
+        if (t==1) update(a,b-v[a]),v[a]=b;
+        else cout << query(b)-query(a-1) << "\n";
+    }
+}

f027717c8904736968fbea2e799a0b74fa4f73d4
Roshan Bellary <roshan.bellary@gmail.com>
2021-12-22 21:13:06 -0800

    CSES Solutions

---
+++ CSES/Range_Queries/ForestQueries.cpp
@@ -0,0 +1,78 @@
+/*
+Problem: Forest Queries
+Problem Link: https://cses.fi/problemset/task/1652/
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=getchar();}
+template<typename T, typename ...Types>
+void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 1e3;
+int N, Q, p[MN][MN];
+char g[MN][MN];
+int main(){
+	setIO();rv(N, Q);char n;
+    for (int i=0;i<N;i++){for (int j=0;j<N;j++) rv(g[i][j]); rv(n);}
+    for (int i=0;i<N;i++){
+        for (int j=0;j<N;j++){
+            if (g[i][j]=='*') p[i][j]++;
+            if (i) p[i][j]+=p[i-1][j];
+            if (j) p[i][j]+=p[i][j-1];
+            if (i&&j) p[i][j]-=p[i-1][j-1];
+        }
+    }
+    while (Q--){
+        int x1,y1,x2,y2,num;rv(x1,y1,x2,y2);
+        num = p[--x2][--y2];--x1;--y1;
+        if (x1) num-=p[x1-1][y2];
+        if (y1) num-=p[x2][y1-1];
+        if (x1&&y1) num+=p[x1-1][y1-1];
+        cout << num << "\n";
+    }
+}
---
+++ CSES/Range_Queries/RangeUpdateQueries.cpp
@@ -0,0 +1,83 @@
+/*
+Problem: Range Update Queries
+Problem Link: https://cses.fi/problemset/task/1651
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=getchar();}
+template<typename T, typename ...Types>
+void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 2e5+1;
+ll N, Q, ft[MN];
+void update(int ind, ll v){
+    while (ind<=N){
+        ft[ind]+=v;
+        ind+=(ind&(-ind));
+    }
+}
+ll query(int ind){
+    return (ind?ft[ind]+query(ind-(ind&(-ind))):0);
+}
+int main(){
+	setIO();rv(N,Q);
+    int p = 0;
+    for (int i=1;i<=N;i++){
+        int v; rv(v);update(i,v-p);p=v;
+    }
+    while (Q--){
+        int t;rv(t);
+        if (t==1){
+            ll a, b, u;rv(a,b,u);
+            update(a,u);update(b+1,-u);
+        }else{
+            int k; rv(k);
+            cout << query(k) << "\n";
+        }
+    }
+}

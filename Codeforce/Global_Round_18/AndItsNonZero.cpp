a5f89d3c557b155ef35fd94ef7fa23f96d03bf4b
Roshan Bellary <roshan.bellary@gmail.com>
2021-12-25 05:32:32 -0800

    Codeforce Contest

---
+++ Codeforce/Global_Round_18/AndItsNonZero.cpp
@@ -0,0 +1,84 @@
+/*
+Problem: 
+Problem Link: 
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=getchar();}
+template<typename T, typename ...Types>
+void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 2e5;
+const int MD = 18;
+int T, N;
+int digc[MN+1][MD];
+void setdig(){
+    for (int i=0;i<=MN;i++){
+        for (int d=0;d<MD;d++){
+            if ((i&(1<<d))==0) digc[i][d]++;
+        }
+    }
+    for (int i=0;i<=MN;i++){
+        for (int d=0;d<MD;d++){
+            if (i) digc[i][d]+=digc[i-1][d];
+        }
+    }
+}
+void solve(){
+    int minv=1e9, l, r;cin >> l >> r;
+    for (int i=0;i<18;i++){
+        minv=min(digc[r][i]-digc[l-1][i],minv);
+    }
+    cout << minv << "\n";
+}
+int main(){
+	setIO();cin >> T;setdig();
+    while (T--){
+        solve();
+    }
+}

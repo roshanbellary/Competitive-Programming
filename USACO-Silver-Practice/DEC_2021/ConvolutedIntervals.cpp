31acee41167ed7e4d7ef6756e11c7c186f11d085
Roshan Bellary <40147115+roshanbellary@users.noreply.github.com>
2021-12-25 16:59:25 -0800

    Update ConvolutedIntervals.cpp

--- USACO-Silver-Practice/DEC_2021/ConvolutedIntervals.cpp
+++ USACO-Silver-Practice/DEC_2021/ConvolutedIntervals.cpp
@@ -5,12 +5,11 @@ const int MV = 1e4+2;
 int N, M;
 ll n[MV];
 ll av[5001],bv[5001];
-vector<pair<int,int>> r;
 int main(){
     cin >> N >> M;
     for (int i=0;i<N;i++){
         int a, b; cin >> a >> b;
-        r.push_back({a,b});av[a]++;bv[b]++;
+        av[a]++;bv[b]++;
     }
     for (int i=0;i<=M;i++){
         for (int j=0;j<=M;j++){

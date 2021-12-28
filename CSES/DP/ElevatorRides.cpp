917537ad80e9593450429ca5adf7a2f7f39eccd7
Roshan Bellary <roshan.bellary@gmail.com>
2021-12-28 14:44:18 -0800

    Create ElevatorRides.cpp

---
+++ CSES/DP/ElevatorRides.cpp
@@ -0,0 +1,78 @@
+/*
+Problem: Elevator Rides
+Problem Link: https://cses.fi/problemset/task/1653/
+Notes: Bitmask dp prob
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=getchar();}
+template<typename T, typename ...Types>
+void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 20;
+ll N, X, w[MN];
+pair<ll,ll> dp[1<<MN];
+int main(){
+	setIO();rv(N, X);
+    for (int i=0;i<N;i++) rv(w[i]);
+    dp[0]={0,X+1};
+    for (int i=1;i<(1<<N);i++){
+        dp[i]={N,1e9};
+        for (int j=0;j<N;j++){
+            if ((i&(1<<j))!=0){
+                if (dp[(i^(1<<j))].s+w[j]>X){
+                    pair<ll,ll> res = {dp[(i^(1<<j))].f+1,w[j]};
+                    dp[i]=min(res,dp[i]);
+                }else{
+                    pair<ll,ll> res = {dp[(i^(1<<j))].f,dp[(i^(1<<j))].s+w[j]};
+                    dp[i]=min(res,dp[i]);
+                }
+            }
+        }
+    }
+    cout << dp[(1<<N)-1].f << "\n";
+}

d305360fed4ddaaaa31661690c5e2f66a3977959
Roshan Bellary <roshan.bellary@gmail.com>
2021-12-31 13:50:15 -0800

    Update CountingNumbers.cpp

--- CSES/DP/CountingNumbers.cpp
+++ CSES/DP/CountingNumbers.cpp
@@ -1,3 +1,9 @@
+
+/*
+Problem: Counting Numbers
+Problem Link: https://cses.fi/problemset/task/2220/
+Notes: Hard dp problem involving digits dp
+*/
 #pragma GCC optimize("O2")
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
@@ -22,20 +28,41 @@ void setIO(string f){
 }
 ll dp[10][18];
 ll a,b;
-ll find(int p){
-	return pow(9,p);
+ll power(ll x, ll y){
+    ll res=1;
+    while(y>0){
+        if (y&1) res= (res*x); y=y>>1; x=(x*x);
+    }
+    return res;
 }
-ll solve(ll n){
-	string s = to_string(n); int l = s.length();
-	ll num = 0;
-	for (int i=0;i<l;i++){
-		for (int j=1;j<(s[i]-'0');j++){
-			num+=find(l);
+ll solve(ll x) {
+	if (x < 0) return 0;
+	if (x == 0) return 1;
+	ll f = 1;
+	while(f) {
+		f = 0;
+		for (ll i = log10(x) - 1; i >= 0; i--) {
+			ll m = power(10, i);
+			if (x/(m*10)%10 == x/m%10) {
+				x = (x/m - 1)*m + (m-1);
+				f = 1;
+			}
 		}
 	}
+	ll N = log10(x)+1,dp[N+1][2],ans = 1,m=1;
+	for (int i=0;i<=N;i++) dp[i][0]=dp[i][1]=0;
+	dp[0][0] = 1, dp[0][1] = 1;
+	for (ll i=1;i<N;i++,m*=10) {
+		dp[i][0]=power(9, i);
+		dp[i][1]=(x/m%10)*dp[i-1][0]+dp[i-1][1];
+		if (x/m%10>x/(m*10)%10)dp[i][1]-=dp[i-1][0];
+		ans+=dp[i][0];
+	}
+	dp[N][1]=(x/m%10-1)*dp[N-1][0]+dp[N-1][1];
+   	return ans+dp[N][1];
 }
 int main(){
 	setIO();
 	cin >> a >> b;
-	
+	cout << solve(b)-solve(a-1) << "\n";
 }

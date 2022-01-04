04bbb3015c830444e10ec40cbc599015d2c0696f
Roshan Bellary <roshan.bellary@gmail.com>
2022-01-03 17:47:52 -0800

    Independent Set Solution

---
+++ Atcoder/DP_Practice/IndependentSet.cpp
@@ -0,0 +1,74 @@
+/*
+Problem: Independent Set
+Problem Link: https://atcoder.jp/contests/dp/tasks/dp_p
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=getchar();}
+template<typename T, typename ...Types>
+inline void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 1e5;
+int N;vector<int> l[MN];
+ll dp[MN][2];
+void dfs1(int ind, int p){
+    dp[ind][0]=dp[ind][1]=1;
+    for (int& j:l[ind]){
+        if (j!=p){
+            dfs1(j,ind);
+            (dp[ind][1]*=((dp[j][0]+dp[j][1])%mod))%=mod;
+            (dp[ind][0]*=(dp[j][1]))%=mod;
+        }
+    }
+}
+int main(){
+	setIO();rv(N);
+    for (int i=0;i<N-1;i++){int u,v;rv(u,v);l[--u].eb(--v);l[v].eb(u);}
+    dfs1(0,0);(dp[0][0]+=dp[0][1])%=mod;
+    cout << dp[0][0] << "\n";
+}

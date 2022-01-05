54b38f48db786c754d923dbb9848f40b99a83945
Roshan Bellary <roshan.bellary@gmail.com>
2022-01-05 14:00:01 -0800

    LCS Solution

--- Atcoder/DP_Practice/LCS.cpp
+++ Atcoder/DP_Practice/LCS.cpp
@@ -1,3 +1,8 @@
+/*
+Problem: LCS
+Problem Link: https://atcoder.jp/contests/dp/tasks/dp_f
+Notes: 
+*/
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
@@ -16,24 +21,24 @@ void setIO(string f){
 	freopen((f+".out").c_str(),"w",stdout);
 	setIO();
 }
-string s,t;
+string s,t, res="";
+int dp[3001][3001];
 int main(){
 	setIO();
 	cin >> s >> t;
-	int ls = s.length();
-	int lt  = t.length();
-	int l = min(ls,lt);
-	int dp[l];
-	for (int i=0;i<l;i++){
-		if ((i==0)){
-			if ((s[i]==t[i]))dp[i]=1;
-			else dp[i]=0;
-		}else{
-			if (s[i]==t[i]){
-				dp[i]=dp[i-1]+1;
-			}else{
-				dp[i]=0;
-			}
+	if (s.length()>t.length()) swap(s,t);
+	for (int i=1;i<=s.length();i++){
+		for (int j=1;j<=t.length();j++){
+			if (s[i-1]==t[j-1]) dp[i][j]=dp[i-1][j-1]+1;
+			else dp[i][j]=max({dp[i][j],dp[i-1][j],dp[i][j-1]});
 		}
 	}
+	int st = s.size(), fi = t.size();
+	while (st && fi){
+		if (dp[st-1][fi-1]+1==dp[st][fi] && dp[st-1][fi]+1==dp[st][fi] && dp[st][fi-1]+1==dp[st][fi]){
+			st--;res=s[st]+res;fi--;
+		}else if (dp[st][fi-1]>dp[st-1][fi])  fi--;
+		else st--;
+	}
+	cout << res << "\n";
 }

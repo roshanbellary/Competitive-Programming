332c9305e1d71b97fef04f4d33443fc0819504c4
Roshan Bellary <roshan.bellary@gmail.com>
2022-01-23 10:28:57 -0800

    Atcoder Beginner Contest 236

---
+++ Atcoder/Beg_Contest_236/Dance.cpp
@@ -0,0 +1,81 @@
+/*
+Problem: 
+Problem Link: 
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=' ';while (c==' '|| c=='\n' || c==EOF) c=getchar();}
+template<typename T, typename ...Types>
+inline void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 8;
+ll N, g[2*MN][2*MN], MV = 0;
+vector<ll> v,aff;
+int counts = 0;
+void iterate(){
+    if (v.size()==0){
+        ll cv = 0;
+        for (ll& i:aff) (cv=(cv^i));
+        MV=max(MV,cv);
+    }else{
+        int fv = v[0];v.erase(v.begin());
+        for (int i=0;i<v.size();i++){
+            int cv = v[i];aff.eb(g[fv][cv]);
+            v.erase(v.begin()+i);
+            iterate();v.eb(cv);aff.pop_back();
+            sort(v.begin(),v.end());
+        }
+        v.eb(fv);sort(v.begin(),v.end());
+    }
+}
+int main(){
+	setIO();rv(N);
+    for (int i=0;i<2*N-1;i++) for (int j=0;j<2*N-i-1;j++) rv(g[i][j+i+1]);
+    for (int i=0;i<2*N;i++) v.eb(i);
+    iterate();cout << MV << "\n";
+}

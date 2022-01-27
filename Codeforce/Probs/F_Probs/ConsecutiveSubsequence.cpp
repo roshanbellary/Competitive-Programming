be73e247d3f798be43c52a3725e98802e57b558c
Roshan Bellary <roshan.bellary@gmail.com>
2022-01-27 08:42:59 -0800

    Consecutive Subsequences Solution

---
+++ Codeforce/Probs/F_Probs/ConsecutiveSubsequence.cpp
@@ -0,0 +1,84 @@
+/*
+Problem: 
+Problem Link: 
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=' ';while (c==' '|| c=='\n' || c==EOF) c=getchar();}
+template<typename T, typename ...Types>
+inline void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 2e5;
+struct Node{int v=-1;};
+int N, pi[MN], v[MN], dp[MN], MV = 0, ind = -1;
+map<int,Node> m;vector<int> res;
+int main(){
+	setIO();rv(N);
+    for (int i=0;i<N;i++) rv(v[i]);
+    for (int i=0;i<N;i++){
+        if (m[v[i]-1].v<0) pi[i]=-1;
+        else pi[i]=m[v[i]-1].v;
+        m[v[i]].v=i; 
+    }
+    dp[0]=1;
+    for (int i=1;i<N;i++){
+        dp[i]=1;
+        if (pi[i]<0) continue;
+        else dp[i]=(dp[pi[i]]+1);
+    }
+    for (int i=0;i<N;i++) if (MV<dp[i]) ind=i,MV=dp[i];
+    cout << MV << "\n";
+    while (true){
+        res.pb(ind+1);
+        if (pi[ind]<0) break;
+        else ind=pi[ind];
+    }
+    reverse(res.begin(),res.end());
+    for (int& j:res) cout << j << " ";
+    cout << "\n";
+}

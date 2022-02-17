570d24fb42810c15bd936cb131f93f4d12b2b68d
Roshan Bellary <roshan.bellary@gmail.com>
2022-02-17 13:54:33 -0800

    Atcoder Regular Contest

---
+++ Atcoder/Reg_Contest_135/FloorCeilDecomp.cpp
@@ -0,0 +1,90 @@
+/*
+██████╗░░█████╗░██████╗░███████╗██╗░░░░░██╗░░░░░
+██╔══██╗██╔══██╗██╔══██╗██╔════╝██║░░░░░██║░░░░░
+██████╔╝██║░░██║██████╦╝█████╗░░██║░░░░░██║░░░░░
+██╔══██╗██║░░██║██╔══██╗██╔══╝░░██║░░░░░██║░░░░░
+██║░░██║╚█████╔╝██████╦╝███████╗███████╗███████╗
+╚═╝░░╚═╝░╚════╝░╚═════╝░╚══════╝╚══════╝╚══════╝
+Problem: 
+Problem Link: 
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod= 998244353;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=' ';while (c==' '|| c=='\n' || c==EOF) c=getchar();}
+template<typename T, typename ...Types>
+inline void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+ll X;
+queue<ll> q;
+unordered_map<ll,pair<ll,ll>> m;
+void dfs(ll X){
+    pair<ll,ll> r = m[X];
+    if (r.f==0 && r.s==0){
+        ll lb = X/2, ub = (X+1)/2;
+        dfs(lb);dfs(ub);
+        r.f=(m[lb].f+m[ub].f);r.s=(m[ub].s+m[lb].s);
+        m[X]=r;
+    }
+}
+ll modpower(ll b, ll p){
+    if (p==0) return 1;
+    if (p%2==1){
+        ll v = modpower(b,p/2);
+        return (((v*v)%mod)*b)%mod;
+    }else{
+        ll v = modpower(b,p/2);
+        return (v*v)%mod;
+    }
+}
+int main(){
+	setIO();rv(X);
+    if (X<=4){cout << X << "\n"; return 0;}
+    m[2]={1,0}; m[3]={0,1}; m[4]={2,0}; m[5]={1,1};
+    dfs(X);
+    cout << (modpower(2,m[X].f)*modpower(3,m[X].s))%mod << "\n";
+}

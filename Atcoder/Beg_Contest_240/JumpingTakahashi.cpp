103598fc197f8bc27f797ed6f084549b86253be9
Roshan Bellary <roshan.bellary@gmail.com>
2022-02-20 18:34:49 -0800

    Adding Beginner Contest 240

---
+++ Atcoder/Beg_Contest_240/JumpingTakahashi.cpp
@@ -0,0 +1,78 @@
+/*
+██████╗░░█████╗░██████╗░███████╗██╗░░░░░██╗░░░░░
+██╔══██╗██╔══██╗██╔══██╗██╔════╝██║░░░░░██║░░░░░
+██████╔╝██║░░██║██████╦╝█████╗░░██║░░░░░██║░░░░░
+██╔══██╗██║░░██║██╔══██╗██╔══╝░░██║░░░░░██║░░░░░
+██║░░██║╚█████╔╝██████╦╝███████╗███████╗███████╗
+╚═╝░░╚═╝░╚════╝░╚═════╝░╚══════╝╚══════╝╚══════╝
+Problem: 
+Problem Link: 
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=' ';while (c==' '|| c=='\n' || c==EOF) c=getchar();}
+template<typename T, typename ...Types>
+inline void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 100, MS = 1e4+1;
+int N, X;
+pair<int,int> ab[MN];
+bool v[MS];
+int main(){
+	setIO();rv(N, X);
+    for (int i=0;i<N;i++) rv(ab[i].f,ab[i].s);
+    v[0]=1;
+    for (int i=0;i<N;i++){
+        for (int j=X;j>=0;j--){
+            if (v[j] && j+ab[i].f<=X) v[j+ab[i].f]=1;
+            if (v[j] && j+ab[i].s<=X) v[j+ab[i].s]=1;
+            v[j]=0;
+        }
+    }
+    cout << (v[X]?"Yes\n":"No\n");
+}
---
+++ Atcoder/Beg_Contest_240/StrangeBalls.cpp
@@ -0,0 +1,84 @@
+/*
+██████╗░░█████╗░██████╗░███████╗██╗░░░░░██╗░░░░░
+██╔══██╗██╔══██╗██╔══██╗██╔════╝██║░░░░░██║░░░░░
+██████╔╝██║░░██║██████╦╝█████╗░░██║░░░░░██║░░░░░
+██╔══██╗██║░░██║██╔══██╗██╔══╝░░██║░░░░░██║░░░░░
+██║░░██║╚█████╔╝██████╦╝███████╗███████╗███████╗
+╚═╝░░╚═╝░╚════╝░╚═════╝░╚══════╝╚══════╝╚══════╝
+Problem: 
+Problem Link: 
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=' ';while (c==' '|| c=='\n' || c==EOF) c=getchar();}
+template<typename T, typename ...Types>
+inline void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 2e5;
+int N, v;
+pair<int,int> vc;
+stack<pair<int,int>> q;
+int main(){
+	setIO();rv(N);
+    for (int i=0;i<N;i++){
+        rv(v);
+        if (q.size()==0){
+            vc.f=v;vc.s=1;
+            q.push({v,1});
+        }else{
+            if (v==vc.f) vc.s++, q.push({vc.f,vc.s});
+            else vc.f=v, vc.s=1, q.push({vc.f,1});
+            if (vc.f==vc.s){
+                while (vc.s--) q.pop();
+                if (!q.empty()) vc.f=q.top().f, vc.s=q.top().s;
+                else vc.f=vc.s=0;
+            }
+        }
+        cout << q.size() << "\n";
+    }
+}

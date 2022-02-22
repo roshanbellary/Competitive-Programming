e1f8846e687ee7f2e4ecec152f6d1c1f384839f4
Roshan Bellary <roshan.bellary@gmail.com>
2022-02-21 16:25:24 -0800

    Adding Codeforce Global Round 19 Solutions

---
+++ Codeforce/Global_Round_19/YetAnotherMinimizationProb.cpp
@@ -0,0 +1,87 @@
+/*
+██████╗░░█████╗░██████╗░███████╗██╗░░░░░██╗░░░░░
+██╔══██╗██╔══██╗██╔══██╗██╔════╝██║░░░░░██║░░░░░
+██████╔╝██║░░██║██████╦╝█████╗░░██║░░░░░██║░░░░░
+██╔══██╗██║░░██║██╔══██╗██╔══╝░░██║░░░░░██║░░░░░
+██║░░██║╚█████╔╝██████╦╝███████╗███████╗███████╗
+╚═╝░░╚═╝░╚════╝░╚═════╝░╚══════╝╚══════╝╚══════╝
+Problem: 
+Problem Link: 
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define f first
+#define s second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=' ';while (c==' '|| c=='\n' || c==EOF) c=getchar();}
+template<typename T, typename ...Types>
+inline void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 100, MS = 1e4+1;
+int T, N, a[MN], b[MN];
+bool r[MS][MN];
+void solve(){
+    rv(N);
+    for (int i=0;i<N;i++) rv(a[i]);
+    for (int i=0;i<N;i++) rv(b[i]);
+    if (N==1){cout << "0\n"; return;}
+    for (int i=0;i<N;i++) for (int j=0;j<MS;j++) r[j][i]=0;
+    r[a[0]][0]=r[b[0]][0]=1;
+    for (int i=1;i<N;i++){
+        for (int j=0;j<MS-b[i];j++) if (r[j][i-1]) r[j+b[i]][i]=1;
+        for (int j=0;j<MS-a[i];j++) if (r[j][i-1]) r[j+a[i]][i]=1;
+    }
+    ll ov = 0, sum = 0;
+    for (int i=0;i<N;i++) ov+=a[i]*a[i], ov+=b[i]*b[i], sum+=(a[i]+b[i]);
+    ov*=(N-2);
+    ll minv = 1e18;
+    for (int i=0;i<MS;i++) if (r[i][N-1]) minv=min(i*i+(sum-i)*(sum-i),minv);
+    ov+=minv;
+    cout << ov << "\n";
+}
+int main(){
+	setIO();rv(T);
+    while (T--) solve();
+}

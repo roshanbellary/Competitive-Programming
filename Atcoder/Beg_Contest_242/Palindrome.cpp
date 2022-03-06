6366721ac7eb89ace2d2f725bcadac839a9e4cf2
Roshan Bellary <roshan.bellary@gmail.com>
2022-03-06 10:31:02 -0800

    Adding (∀x∀) solution

---
+++ Atcoder/Beg_Contest_242/Palindrome.cpp
@@ -0,0 +1,79 @@
+/*
+██████╗░░█████╗░██████╗░███████╗██╗░░░░░██╗░░░░░
+██╔══██╗██╔══██╗██╔══██╗██╔════╝██║░░░░░██║░░░░░
+██████╔╝██║░░██║██████╦╝█████╗░░██║░░░░░██║░░░░░
+██╔══██╗██║░░██║██╔══██╗██╔══╝░░██║░░░░░██║░░░░░
+██║░░██║╚█████╔╝██████╦╝███████╗███████╗███████╗
+╚═╝░░╚═╝░╚════╝░╚═════╝░╚══════╝╚══════╝╚══════╝
+Problem: (∀x∀)
+Problem Link: https://atcoder.jp/contests/abc242/tasks/abc242_e
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define x first
+#define y second
+int mod=998244353;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=' ';while (c==' '|| c=='\n' || c==EOF) c=getchar();}
+template<typename T, typename ...Types>
+inline void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+int T, N;
+string S;
+void solve(){
+    rv(N, S);ll col = 0;
+    for (int i=0;i<(N+1)/2;i++){
+        (col*=26)%=mod;(col+=(S[i]-'A'))%=mod;
+    }
+    ll w = 1;
+    for (int i=(N-1)/2;i>=0;i--){
+        if (S[i]<S[N-1-i]) break;
+        if (S[i]>S[N-1-i]){w=0;break;}
+    }
+    (col+=w)%=mod;cout << col << "\n";
+}
+int main(){
+	setIO();rv(T);
+    while (T--) solve();
+}

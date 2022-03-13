c4cb9c9540f2380ab060fe1c043e4d7561b5d475
Roshan Bellary <roshan.bellary@gmail.com>
2022-03-13 08:11:15 -0700

    Beginner Contest 243 Solutions

---
+++ Atcoder/Beg_Contest_243/Collision2.cpp
@@ -0,0 +1,80 @@
+/*
+██████╗░░█████╗░██████╗░███████╗██╗░░░░░██╗░░░░░
+██╔══██╗██╔══██╗██╔══██╗██╔════╝██║░░░░░██║░░░░░
+██████╔╝██║░░██║██████╦╝█████╗░░██║░░░░░██║░░░░░
+██╔══██╗██║░░██║██╔══██╗██╔══╝░░██║░░░░░██║░░░░░
+██║░░██║╚█████╔╝██████╦╝███████╗███████╗███████╗
+╚═╝░░╚═╝░╚════╝░╚═════╝░╚══════╝╚══════╝╚══════╝
+Problem: 
+Problem Link: 
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define x first
+#define y second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=' ';while (c==' '|| c=='\n' || c==EOF) c=getchar();}
+template<typename T, typename ...Types>
+inline void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 2e5;
+int N;
+pair<int,int> pos[MN];
+string L;
+map<int,vector<pair<int,char>>> m;
+bool col = 0;
+int main(){
+	setIO();rv(N);
+    for (int i=0;i<N;i++) rv(pos[i].x,pos[i].y);
+    rv(L);
+    for (int i=0;i<N;i++) m[pos[i].y].pb({pos[i].x,L[i]});
+    for (auto& a:m) sort(a.y.begin(),a.y.end());
+    for (auto& a:m){
+        for (int i=0;i<a.y.size()-1;i++){
+            if (a.y[i].y!=a.y[i+1].y && a.y[i].y=='R') col=1;
+        }
+    }
+    cout << (col?"Yes":"No") << "\n";
+}

4301be404daa7bf48a4e31f8ae06128384b06c64
Roshan Bellary <roshan.bellary@gmail.com>
2022-03-18 09:40:18 -0700

    Integral Array Solution

---
+++ Codeforce/Probs/B_Probs/IntegralArray.cpp
@@ -0,0 +1,85 @@
+/*
+██████╗░░█████╗░██████╗░███████╗██╗░░░░░██╗░░░░░
+██╔══██╗██╔══██╗██╔══██╗██╔════╝██║░░░░░██║░░░░░
+██████╔╝██║░░██║██████╦╝█████╗░░██║░░░░░██║░░░░░
+██╔══██╗██║░░██║██╔══██╗██╔══╝░░██║░░░░░██║░░░░░
+██║░░██║╚█████╔╝██████╦╝███████╗███████╗███████╗
+╚═╝░░╚═╝░╚════╝░╚═════╝░╚══════╝╚══════╝╚══════╝
+Problem: Integral Array
+Problem Link: https://codeforces.com/problemset/problem/1648/B
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define x first
+#define y second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=' ';while (c==' '|| c=='\n' || c==EOF) c=getchar();}
+template<typename T, typename ...Types>
+inline void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 1e6+1;
+ll T, N, C, a[MN], v[MN];
+bool pr[MN];
+void solve(){
+    rv(N, C);map<int,int> p;
+    for (int i=0;i<N;i++) rv(a[i]), p[a[i]]++, v[a[i]]++, pr[a[i]]=1;
+    for (int i=1;i<=C;i++) v[i]+=v[i-1];
+    bool w = 1;
+    for (ll i=1;i<=C;i++){
+        if (pr[i]>0){
+            for (ll j=i;j<=C;j+=i){
+                if (pr[j/i]) continue;
+                else if (v[min(j+i-1,C)]-v[j-1]) w=0;
+            }
+        }
+    }
+    for (int i=0;i<=C;i++) v[i]=0;
+    for (int i=0;i<N;i++) pr[a[i]]=0;
+    cout << (w?"Yes":"No") << "\n";
+}
+int main(){
+	setIO();rv(T);
+    while (T--) solve();
+}

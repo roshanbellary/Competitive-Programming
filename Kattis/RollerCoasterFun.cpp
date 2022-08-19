1d2c8f8493b0038da3dd0cd8b0e69649f365d2ea
Roshan Bellary <roshan.bellary@gmail.com>
2022-08-19 09:35:25 -0700

    Adding Roller Coaster Fun Solution

---
+++ Kattis/RollerCoasterFun.cpp
@@ -0,0 +1,96 @@
+/*
+██████╗░░█████╗░██████╗░███████╗██╗░░░░░██╗░░░░░
+██╔══██╗██╔══██╗██╔══██╗██╔════╝██║░░░░░██║░░░░░
+██████╔╝██║░░██║██████╦╝█████╗░░██║░░░░░██║░░░░░
+██╔══██╗██║░░██║██╔══██╗██╔══╝░░██║░░░░░██║░░░░░
+██║░░██║╚█████╔╝██████╦╝███████╗███████╗███████╗
+╚═╝░░╚═╝░╚════╝░╚═════╝░╚══════╝╚══════╝╚══════╝
+Problem: Roller Coaster Fun
+Problem Link: https://open.kattis.com/problems/rollercoasterfun
+Notes: 
+*/
+#pragma GCC optimize("O2")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+typedef long long ll;
+#define pb push_back
+#define eb emplace_back
+#define countbits __builtin_popcount
+#define beg0 __builtin_clz
+#define terminal0 __builtin_ctz
+#define x first
+#define y second
+int mod=1e9+7;
+inline void rv(int &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(ll &n){
+    n=0;int m=1;char c=getchar();if (c=='-'){m=-1; c=getchar();}
+    for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    n*=m;
+}
+inline void rv(double &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(float &n){
+    n=0;int m=1;char c=getchar();
+    if (c=='-'){m=-1; c=getchar();}for (;c>47 && c<58;c=getchar()){n=n*(1<<1)+n*(1<<3)+c-48;}
+    if (c=='.'){double p = 0.1;c=getchar();for (;c>47 && c<58;c=getchar()){n+=((c-48)*p);p/=10;}}
+    n*=m;
+}
+inline void rv(string &w){w="";char c=getchar();while (c!=' '&&c!='\n'&&c!=EOF){w+=c;c=getchar();}}
+inline void rv(char &c){c=' ';while (c==' '|| c=='\n' || c==EOF) c=getchar();}
+template<typename T, typename ...Types>
+inline void rv(T &n, Types&&... args){rv(n);rv(args...);}
+void setIO(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+void setIO(string f){
+	freopen((f+".in").c_str(),"r",stdin);
+	freopen((f+".out").c_str(),"w",stdout);
+	setIO();
+}
+const int MN = 100, MT = 25001;
+struct Ride{
+    ll a, b, t;
+    bool sort(const Ride &n) const{
+        return (b<n.b);
+    }
+};
+int N, Q;
+Ride r[MN];
+ll dp[MT];
+int main(){
+	setIO();rv(N);
+    for (int i=0;i<N;i++){
+        rv(r[i].a,r[i].b,r[i].t);
+    }
+    sort(r,r+N);
+    for (int i=0;i<N;i++){
+        for (int t=MT-r[i].t-1;t>=0;t--){
+            ll c = 1;
+            ll col = 0;
+            while (1){
+                if (t+c*r[i].t>=MT || (r[i].a-r[i].b*(c-1)*(c-1))<=0) break;
+                col+=(r[i].a-r[i].b*(c-1)*(c-1));
+                dp[t+c*r[i].t]=max(dp[t+c*r[i].t], dp[t]+col);
+                c++;
+            }
+        }
+    }
+    for (int i=0;i<MT;i++) if (i) dp[i]=max(dp[i],dp[i-1]);
+    rv(Q);
+    while (Q--){
+        int T; rv(T);
+        cout << dp[T] << "\n";
+    }
+}
